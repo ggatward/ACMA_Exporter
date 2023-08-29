23ce46c99b33dd144cc1096e6f7d2b0266d13fda
Porky Pig <porky@boats.float>
2023-08-29 20:24:58 +1000

    Add airservices

--- getdata.py
+++ getdata.py
@@ -6,9 +6,9 @@ import requests, sys, os, glob
 import datetime, tempfile, yaml, csv
 
 # pip install geopandas PyShp shapely
-import geopandas as gpd
-import shapefile
-from shapely.geometry import Point, Polygon, MultiPolygon, shape
+# import geopandas as gpd
+# import shapefile
+# from shapely.geometry import Point, Polygon, MultiPolygon, shape
 
 
 # request properties
@@ -127,111 +127,111 @@ def getRegistrations(licenceList,clientId):
     f.close()
 
 
-def extractPolys(index,clientId,shpmeta,service):
-    # From:  https://gis.stackexchange.com/questions/113799/how-to-read-a-shapefile-in-python
-    polylist = []
+# def extractPolys(index,clientId,shpmeta,service):
+#     # From:  https://gis.stackexchange.com/questions/113799/how-to-read-a-shapefile-in-python
+#     polylist = []
 
-    # Extract specific shape id in GeoJSON format
-    data = shpdata.shapeRecords()[index]
-    area_data = data.shape.__geo_interface__
-    area_name = shpmeta['RDA'][index].replace(" ","_")
-    # Aggregate smaller areas
-    if 'Illawarra' in area_name:
-        area_name = 'NSW_South_Coast'
-    if 'Southern_Inland' in area_name:
-        area_name = 'NSW_South_Coast'
-    if 'South_Coast' in area_name:
-        area_name = 'NSW_South_Coast'
-    if 'Australian_Capital_Territory' in area_name:
-        area_name = 'NSW_South_Coast'
-    if 'Central_West' in area_name:
-        area_name = 'NSW_South_Coast'
-    if 'Central_Coast' in area_name:
-        area_name = 'Hunter_Syd'
-    if 'Hunter' in area_name:
-        area_name = 'Hunter_Syd'
-    if 'Sydney' in area_name:
-        area_name = 'Hunter_Syd'
-    if 'Northern_Inland' in area_name:
-        area_name = 'NSW_North_Coast'
-    if 'Northern_Rivers' in area_name:
-        area_name = 'NSW_North_Coast'
-    if 'Mid_North_Coast' in area_name:
-        area_name = 'NSW_North_Coast'
-    if 'Orana' in area_name:
-        area_name = 'Orana_Far_West'
-    if 'Far_West' in area_name:
-        area_name = 'Orana_Far_West'
-    if 'Riverina' in area_name:
-        area_name = 'Riverina_Murray'
-    if 'Murray' in area_name:
-        area_name = 'Riverina_Murray'
+#     # Extract specific shape id in GeoJSON format
+#     data = shpdata.shapeRecords()[index]
+#     area_data = data.shape.__geo_interface__
+#     area_name = shpmeta['RDA'][index].replace(" ","_")
+#     # Aggregate smaller areas
+#     if 'Illawarra' in area_name:
+#         area_name = 'NSW_South_Coast'
+#     if 'Southern_Inland' in area_name:
+#         area_name = 'NSW_South_Coast'
+#     if 'South_Coast' in area_name:
+#         area_name = 'NSW_South_Coast'
+#     if 'Australian_Capital_Territory' in area_name:
+#         area_name = 'NSW_South_Coast'
+#     if 'Central_West' in area_name:
+#         area_name = 'NSW_South_Coast'
+#     if 'Central_Coast' in area_name:
+#         area_name = 'Hunter_Syd'
+#     if 'Hunter' in area_name:
+#         area_name = 'Hunter_Syd'
+#     if 'Sydney' in area_name:
+#         area_name = 'Hunter_Syd'
+#     if 'Northern_Inland' in area_name:
+#         area_name = 'NSW_North_Coast'
+#     if 'Northern_Rivers' in area_name:
+#         area_name = 'NSW_North_Coast'
+#     if 'Mid_North_Coast' in area_name:
+#         area_name = 'NSW_North_Coast'
+#     if 'Orana' in area_name:
+#         area_name = 'Orana_Far_West'
+#     if 'Far_West' in area_name:
+#         area_name = 'Orana_Far_West'
+#     if 'Riverina' in area_name:
+#         area_name = 'Riverina_Murray'
+#     if 'Murray' in area_name:
+#         area_name = 'Riverina_Murray'
 
-    if 'Loddon_Mallee' in area_name:
-        area_name = 'VIC_West'
-    if 'Grampians' in area_name:
-        area_name = 'VIC_West'
-    if 'Barwon' in area_name:
-        area_name = 'VIC_West'
-    if 'Hume' in area_name:
-        area_name = 'VIC_East'
-    if 'Gippsland' in area_name:
-        area_name = 'VIC_East'
+#     if 'Loddon_Mallee' in area_name:
+#         area_name = 'VIC_West'
+#     if 'Grampians' in area_name:
+#         area_name = 'VIC_West'
+#     if 'Barwon' in area_name:
+#         area_name = 'VIC_West'
+#     if 'Hume' in area_name:
+#         area_name = 'VIC_East'
+#     if 'Gippsland' in area_name:
+#         area_name = 'VIC_East'
 
-    if 'Logan_and_Redlands' in area_name:
-        area_name = 'Brisbane'
-    if 'Brisbane_City' in area_name:
-        area_name = 'Brisbane'
-    if 'Moreton_Bay' in area_name:
-        area_name = 'Brisbane'
-    if 'Ipswich_and_West_Moreton' in area_name:
-        area_name = 'Brisbane'
-    if 'Gold_Coast' in area_name:
-        area_name = 'Brisbane'
-    if 'Sunshine_Coast' in area_name:
-        area_name = 'Wide_Bay_Burnett'
+#     if 'Logan_and_Redlands' in area_name:
+#         area_name = 'Brisbane'
+#     if 'Brisbane_City' in area_name:
+#         area_name = 'Brisbane'
+#     if 'Moreton_Bay' in area_name:
+#         area_name = 'Brisbane'
+#     if 'Ipswich_and_West_Moreton' in area_name:
+#         area_name = 'Brisbane'
+#     if 'Gold_Coast' in area_name:
+#         area_name = 'Brisbane'
+#     if 'Sunshine_Coast' in area_name:
+#         area_name = 'Wide_Bay_Burnett'
 
-    # If we have a MultiPolygon we need to split it into individual Polygons
-    if (shape(area_data).type == 'MultiPolygon'):
-        for polygon in shape(area_data):
-            polylist.append(polygon)
-    else:
-        polylist.append(area_data)
+#     # If we have a MultiPolygon we need to split it into individual Polygons
+#     if (shape(area_data).type == 'MultiPolygon'):
+#         for polygon in shape(area_data):
+#             polylist.append(polygon)
+#     else:
+#         polylist.append(area_data)
 
-    # Open a file for STATEWIDE frequencies
-    statewide = open('output/' + service + '_Statewide.csv', 'a')
+#     # Open a file for STATEWIDE frequencies
+#     statewide = open('output/' + service + '_Statewide.csv', 'a')
 
-    # Loop through each polygon
-    for i in polylist:
-        # Convert poly to shapely format
-        coords = shape(i)
-        poly = Polygon(coords)
+#     # Loop through each polygon
+#     for i in polylist:
+#         # Convert poly to shapely format
+#         coords = shape(i)
+#         poly = Polygon(coords)
 
-        # Open a file to save the results
-        lf = open('output/' + service + '_' + area_name + '.csv', 'a')
+#         # Open a file to save the results
+#         lf = open('output/' + service + '_' + area_name + '.csv', 'a')
 
-        # Loop through each site in the freqlist looking for sites inside the current poly
-        with open('output/' + clientId + '.csv', newline='') as csvfile:
-            data = csv.DictReader(csvfile)
-            for row in data:
-                # Extract the lon/lat and set a shapely 'point'
-                point = Point(float(row['lon']),float(row['lat']))
+#         # Loop through each site in the freqlist looking for sites inside the current poly
+#         with open('output/' + clientId + '.csv', newline='') as csvfile:
+#             data = csv.DictReader(csvfile)
+#             for row in data:
+#                 # Extract the lon/lat and set a shapely 'point'
+#                 point = Point(float(row['lon']),float(row['lat']))
 
-                # Check if the point is in the poly - True/False
-                if (point.within(poly)):
-                    # Write out our region matching CSV
-                    print(row['frequency'] + " at " + row['location'] + " is in '" + area_name + "'")
-                    lf.write(row['frequency'] + "," + row['location'] + "," + row['lat'] + "," + row['lon'] +"," + row['mode'] + "\n")
-                if (row['location'] == 'STATEWIDE'):
-                    out = True
-                    statewide.write(row['frequency'] + "," + row['location'] + "," + row['lat'] + "," + row['lon'] +"," + row['mode'] + "\n")
-        lf.close()
-    statewide.close()
-    # Remove all duplicates from statewide file
-    uniqlines = set(open('output/' + service + '_Statewide.csv').readlines())
-    #bar = open('output/' + service + '_Statewide1.csv', 'w').writelines(set(uniqlines))
-    bar = open('output/' + service + '_Statewide.csv', 'w').writelines(set(uniqlines))
-    #bar.close()
+#                 # Check if the point is in the poly - True/False
+#                 if (point.within(poly)):
+#                     # Write out our region matching CSV
+#                     print(row['frequency'] + " at " + row['location'] + " is in '" + area_name + "'")
+#                     lf.write(row['frequency'] + "," + row['location'] + "," + row['lat'] + "," + row['lon'] +"," + row['mode'] + "\n")
+#                 if (row['location'] == 'STATEWIDE'):
+#                     out = True
+#                     statewide.write(row['frequency'] + "," + row['location'] + "," + row['lat'] + "," + row['lon'] +"," + row['mode'] + "\n")
+#         lf.close()
+#     statewide.close()
+#     # Remove all duplicates from statewide file
+#     uniqlines = set(open('output/' + service + '_Statewide.csv').readlines())
+#     #bar = open('output/' + service + '_Statewide1.csv', 'w').writelines(set(uniqlines))
+#     bar = open('output/' + service + '_Statewide.csv', 'w').writelines(set(uniqlines))
+#     #bar.close()
 
 
 def cleanup(service):
@@ -261,10 +261,10 @@ if __name__ == "__main__":
     # Shapefile sample used: https://www.rda.gov.au/sites/default/files/images/rda-map-national-0920.jpg
     infile = "shapefiles/RDA_2015_16.shp"
 
-    # Read in our shapefile for geo-referencing
-    shpdata = shapefile.Reader(infile)
-    # Extract data about the shapefile
-    shpmeta = gpd.read_file(infile)
+    # # Read in our shapefile for geo-referencing
+    # shpdata = shapefile.Reader(infile)
+    # # Extract data about the shapefile
+    # shpmeta = gpd.read_file(infile)
 
 #    print(shpmeta)
 
@@ -289,22 +289,27 @@ if __name__ == "__main__":
 
 
     clients = [
-        { "clientID": "20011941", "service": "ASNSW" },
-        { "clientID": "17661", "service": "ASNSW2" },
-        { "clientID": "37658", "service": "SJA" },
-        { "clientID": "20012532", "service": "SES" },
-        { "clientID": "516364", "service": "SES2" },
-        { "clientID": "20005985", "service": "RFS" },
-        { "clientID": "5832", "service": "RFS2" },
+        #{ "clientID": "20011941", "service": "ASNSW" },
+        #{ "clientID": "17661", "service": "ASNSW2" },
+        #{ "clientID": "37658", "service": "SJA" },
+        #{ "clientID": "20012532", "service": "SES" },
+        #{ "clientID": "516364", "service": "SES2" },
+        #{ "clientID": "20005985", "service": "RFS" },
+        #{ "clientID": "5832", "service": "RFS2" },
         #{ "clientID": "20027621", "service": "OEH" },
         #{ "clientID": "20012756", "service": "Commonwealth_Agencies" },
         #{ "clientID": "20019469", "service": "Low_Power" },
-        { "clientID": "20008471", "service": "FRNSW" },
-        { "clientID": "20020998", "service": "RMS" },
+        #{ "clientID": "20008471", "service": "FRNSW" },
+        #{ "clientID": "20020998", "service": "RMS" },
         #{ "clientID": "20036348", "service": "HGSA" },
-        { "clientID": "115634", "service": "NPWS" },
-        { "clientID": "391222", "service": "Airservices" },
-        { "clientID": "20029221", "service": "QPRC" },
+        #{ "clientID": "115634", "service": "NPWS" },
+        { "clientID": "391222", "service": "Airservices_ACT_NSW" },
+        { "clientID": "389917", "service": "Airservices_VIC_TAS" },
+        { "clientID": "396261", "service": "Airservices_SA_NT" },
+        { "clientID": "401054", "service": "Airservices_QLD" },
+        { "clientID": "399343", "service": "Airservices_WA" },
+        #{ "clientID": "20029221", "service": "QPRC" },
+        { "clientID": "46975", "service": "DoD" },
 
     ]
 
@@ -322,10 +327,10 @@ if __name__ == "__main__":
         getRegistrations(clientLicences,clientId)
 
         # Geolocate sites and split CSV into per client-id per region
-        for index, item in enumerate(shpdata.shapeRecords()):
-            # Area 54 (Northern Territory) seems to be corrupt - skip it.
-            if index != 54:
-                poly = extractPolys(int(index),clientId,shpmeta,service)
+        # for index, item in enumerate(shpdata.shapeRecords()):
+        #     # Area 54 (Northern Territory) seems to be corrupt - skip it.
+        #     if index != 54:
+        #         poly = extractPolys(int(index),clientId,shpmeta,service)
 
         # Finally clean out any empty files in the output dir
         deleteEmptyFiles()
